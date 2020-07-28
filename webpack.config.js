8a66b4279aca07a9958662fc5d243cf591ca8b5b
mekhalajoshi <mekhalajoshi@gmail.com>
2020-07-28 10:27:05 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+// const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'app'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+
+          },
+        ],
+      },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.mjs', '.js', '.json', '.jsx'],
+  },
+  // plugins: [
+  //   new HtmlWebPackPlugin({
+  //     template: `${__dirname}/src/index.html`,
+  //     filename: 'index.html',
+  //     inject: 'body',
+  //   }),
+  // ],
+  devtool: 'cheap-module-eval-source-map',
+};
